078c85f54aeed5896fcd02fa4fb0364b593e82a2
Mayank17M <mamgain.mayank01@gmail.com>
2022-01-31 16:23:17 +0530

    Add actions and reducers

---
+++ time-table/client/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <ul>
+          <li className="logo">
+            <a href="">Time-Table Generator</a>
+          </li>
+          <li className="right navlink">
+            <a href=""> About</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
---
+++ time-table/client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import SignIn from "../SignIn";
+import Register from "../Register";
+import Navbar from "../Navbar";
+import Main from "../Main";
+
+class App extends Component {
+  render() {
+    return (<div>
+      <Navbar />
+      <br />
+      <Register />
+    </div>
+  );
+  }
+}
+
+export default App;
